e2aa23493bdf0f8ba1d501c68384fc72d44d50a4
OsaeAddo <immanueloaddo@gmail.com>
2022-09-18 15:17:24 +0000

    update TransactionContext

--- context/TransactionContext.jsx
+++ context/TransactionContext.jsx
@@ -13,11 +13,14 @@ const getEthContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum)
     const signer = provider.getSigner()
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer)
+    
     console.log({
         provider, 
         signer,
         transactionContract
     })
+
+    return transactionContract
 }
 
 export const TransactionProvider = ({ children }) => {
