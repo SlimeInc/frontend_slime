6fd0e97432615025647896fa8c9f86a6ffd60451
OsaeAddo <immanueloaddo@gmail.com>
2022-09-26 13:53:10 +0000

    update sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,14 @@
-  import { useState } from 'react'
+import { useState, useContext } from 'react'
 import AvatarSection from './AvatarSection'
 import SidebarLinks from './SidebarLinks'
 import styles from './Sidebar.module.scss'
 import Metamask_icon from '../public/images/logos_metamask-icon.png'
 
+import { TransactionContext } from '../context/TransactionContext'
 export const Sidebar = () => {
-const [name, setName] = useState('Metamask Wallet')
+  const {currentAccount} = useContext(TransactionContext)
+  
+  const [name, setName] = useState(currentAccount)
 
   return (
     <div className={styles.sidebar}>
