c7bf60929d45abf893e504623e51a4c207550de8
OsaeAddo <immanueloaddo@gmail.com>
2022-09-27 02:49:33 +0000

    update sendTransaction

--- context/TransactionContext.jsx
+++ context/TransactionContext.jsx
@@ -170,6 +170,7 @@ export const TransactionProvider = ({ children }) => {
             formData, setFormData, 
             handleChange, 
             sendTransaction,
+            transactionCount,
             transactions, getAllTransactions, 
             }}
         >
