d2d17d7e90bfceeeb7e6b8fd4e7c42a1717b26d0
OsaeAddo <immanueloaddo@gmail.com>
2022-09-27 02:36:08 +0000

    define TransactionsFrame

--- pages/transactions.js
+++ pages/transactions.js
@@ -8,15 +8,28 @@ import { useContext } from "react";
 
 const date = new Date();
 
-const data = {
-  brief: true,
-  amount: "3",
-  date: date.getHours(),
-  receiving: true,
-  address: "sf432543fkjskugsiufidsfioasafs",
-  tokens: "Ether",
-};
-function transactions() {
+
+
+const TransactionsFrame = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+  return (
+    <div>
+      <div>
+        <a href={`https://goerli.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+            <p className="text-white text-base">From: {addressFrom}</p>
+        </a>
+        <a href={`https://goerli.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+          <p className="text-white text-base">To: {addressTo}</p>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+
+function Transactions() {
+  const { transactions, currentAccount } = useContext(TransactionContext)
+  
+  console.log("current transactions: ", transactions)
   return (
     <div className={styling.main}>
       <TransactionHead />
